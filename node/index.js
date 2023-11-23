d7666c1686a56604fc8847cf1a37131f79a09580
Rafael Maltez <rafaelmaltez89@gmail.com>
2023-11-23 11:25:18 -0300

    adjust node service, compose and nginx conf file

--- node/index.js
+++ node/index.js
@@ -6,21 +6,19 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
-  const randomName = names[Math.round(Math.random() * names.length -1)];
-
-  await connection.execute('INSERT INTO people(name) VALUES(?)', [randomName]);
-
-  const [insertedNames] = await connection.execute('SELECT name FROM people');
-
-  let htmlString = '<h1>Full Cycle Rocks!</h1><ol>'
-
-  insertedNames.forEach((item) => {
-    htmlString += `<li>${item.name}</li>`
-  })
-
-  htmlString += '</ol>'
-
-  res.send(htmlString);
+  try {
+    const randomName = names[Math.round(Math.random() * names.length -1)];
+    await connection.execute('INSERT INTO people(name) VALUES(?)', [randomName]);
+    const [insertedNames] = await connection.execute('SELECT name FROM people');
+    let htmlString = '<h1>Full Cycle Rocks!</h1><ol>'
+    insertedNames.forEach((item) => {
+      htmlString += `<li>${item.name}</li>`
+    })
+    htmlString += '</ol>'
+    res.send(htmlString);
+  } catch (error) {
+    res.status(500).send('Ops... something went wrong, try again.')
+  }
 })
 
 app.listen(port, () => console.log(`Rodando na porta ${port}`))
